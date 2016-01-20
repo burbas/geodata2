54e8ac7284a6f0fbf939959bb8d3259e24d7148b
Niclas Axelsson <niclas@burbas.se>
2016-01-20 14:40:41 +0100

    Change start_link to not having any arguments

--- src/geodata2.erl
+++ src/geodata2.erl
@@ -5,7 +5,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% API
--export([lookup/1, start/0, start_link/1, stop/0, get_env/2, id/1]).
+-export([lookup/1, start/0, start_link/0, stop/0, get_env/2, id/1]).
 -include("geodata2.hrl").
 
 -record(state, {}).
@@ -24,8 +24,8 @@ lookup(IP) ->
 start() ->
 	application:start(geodata2).
 
-start_link(Name) ->
-    gen_server:start_link({local, Name}, ?MODULE, [], []).
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
 	application:stop(geodata2).
